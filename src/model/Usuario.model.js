071ce61c296bfb11f604e291b1d0f789cd3cf31a
João Vitor Seiji Sato Calvaro <joaovssc@gmail.com>
2025-08-16 14:43:20 -0300

    Feat: Usuário Model

---
+++ src/model/Usuario.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    nome: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    senha: {type: String, required: true},
+    imagem: {type: String, required: true},
+    enderecos: [
+        {
+            rua:{ type: String, required: true},
+            numero: {type: Number, required: true},
+            complemento: {type: String, required: false},
+            CEP: {type: String, required: true},
+            createdAt: {type: Date, required: true}
+        }
+    ],
+    createAt: {type: Date, required: true},
+    produtos_fav: [
+        {
+            _id: {type: mongoose.Schema.Types.ObjectId, required: true, unique: true, ref: "produtos"},
+            createdAt: {type: Date, required: true}
+        }
+    ],
+    admin: {type: Boolean, required: true, default: false}
+});
+
+const User = mongoose.model("usuario", UserSchemac);
+
+module.exports = User;
