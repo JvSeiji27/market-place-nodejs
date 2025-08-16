f12b8654c40bb8772f592a7e0fb6ead44d5942d8
João Vitor Seiji Sato Calvaro <joaovssc@gmail.com>
2025-08-16 16:54:16 -0300

    Feat: UserController atualizado

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -1,16 +1,37 @@
-
+const userService = require("../service/usuario.service")
+const mongoose = require("mongoose")
 const findUserByIdController = async (req, res) => {
     try {
         const id = req.params.id;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).send({ message: "O ID está invalido!" });
+
+        } else {
+            const user = await userService.findUserByIdService(id);
+            if (!user) {
+                return res.status(404).send({ message: "Nenhum usuário encontrado!" });
+
+            }
+            return res.status(200).send(user);
+        }
     } catch (err) {
         console.log(err.message);
         return res.status(500).send({ message: "Erro inesperado tente novamente!" })
     }
 }
 
+//Object.keys(users).length === 0 é para o Create
+
+//O findAll retorna um vetor e um vetor vazio em js não é nulo 
 const findAllUsersController = async (req, res) => {
     try {
+        const users = await userService.findAllUsersService();
+        if (users.length === 0) {
+            return res.status(404).send({ message: "Nenhum usuário foi encontrado" });
+        } else {
+            return res.status(200).send(users);
 
+        }
     } catch (err) {
         console.log(err.message);
         return res.status(500).send({ message: "Erro inesperado tente novamente!" })
@@ -19,7 +40,18 @@ const findAllUsersController = async (req, res) => {
 
 const createUserController = async (req, res) => {
     try {
-
+        let user = req.body;
+        if (!user || Object.keys(user).length === 0) {
+            return res.status(400).send({ message: "Body vazio" })
+        }
+        const parts = ["nome", "email", "senha", "imagem", "admin"];
+        for (const part of parts) {
+            if (!user[part]) {
+                return res.status(400).send(`Este campo "${part}" está vazio`);
+            }
+        }
+        user = await userService.createUserService(user);
+        return res.status(200).send(user);
     } catch (err) {
         console.log(err.message);
         return res.status(500).send({ message: "Erro inesperado tente novamente!" })
@@ -28,6 +60,22 @@ const createUserController = async (req, res) => {
 
 const updateUserController = async (req, res) => {
     try {
+        const id = req.params.id;
+        let user = req.body;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).send({ message: "O ID está invalido!" });
+        }
+
+        if (!user || Object.keys(user).length === 0) {
+            return res.status(400).send({ message: "Body vazio" })
+        }
+        const parts = ["nome", "email", "senha", "imagem", "admin"];
+        for (const part of parts) {
+            if (!user[part]) {
+                return res.status(400).send(`Este campo "${part}" está vazio`);
+            }
+        }
+        user = await userService.updateUserService(id, user);
 
     } catch (err) {
         console.log(err.message);
@@ -37,7 +85,18 @@ const updateUserController = async (req, res) => {
 
 const removeUserController = async (res, req) => {
     try {
+        const id = req.params.id;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).send({ message: "O ID está invalido!" });
+        }
+        const removedUser = await userService.removeUserController(id);
+        if (!removedUser) {
+            return res.status(404).send({ message: "Usuário não encontrado!" });
 
+        } else {
+            return res.status(200).send({ removedUser});
+
+        }
     } catch (err) {
         console.log(err.message);
         return res.status(500).send({ message: "Erro inesperado tente novamente!" })
--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -2,22 +2,24 @@ const express = require("express");
 const  router = express.Router();
 const userController = require("../controller/usuario.controller")
 
+//OBS: todos devem começar com barra
+
 //ROTAS GET
-router.get("findById/:id", userController.findUserByIdController);
-router.get("findAll/", userController.findAllUsersController);
+router.get("/findById/:id", userController.findUserByIdController);
+router.get("/findAll/", userController.findAllUsersController);
 
 //ROTAS POST
-router.post("create/", userController.createUserController);
-router.post("addAddress/:id",userController.addUserAddressController);
-router.post("addFavProduct/:id", userController.addUserFavProductController);
+router.post("/create/", userController.createUserController);
+router.post("/addAddress/:id",userController.addUserAddressController);
+router.post("/addFavProduct/:id", userController.addUserFavProductController);
 
 //ROTAS PUT
-router.put("update/:id", userController.updateUserController);
+router.put("/update/:id", userController.updateUserController);
 
 //ROTAS DELETE
-router.delete("remove/:id", userController.removeUserController);
-router.delete("removeAddress/", userController.removeUserAddressController);
-router.delete("removeFavProduct/", userController.removeUserFavProductController) ;
+router.delete("/remove/:id", userController.removeUserController);
+router.delete("/removeAddress/", userController.removeUserAddressController);
+router.delete("/removeFavProduct/", userController.removeUserFavProductController) ;
 
 
 
