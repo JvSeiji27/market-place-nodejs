3b0ba98ff7c12ff6f9d56c6dea5fcb26fd253fd7
João Vitor Seiji Sato Calvaro <joaovssc@gmail.com>
2025-08-17 21:19:16 -0300

    Feat: adição middleware para rotas do User

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const database = require("./src/database/database") //arquivo de conexão com o banco
 const userRouter = require('./src/router/usuario.router') //arquivo de rota do usuário
+const authRouter = require("./src/router/auth.router")
 const app = express();
 const port = 3000;
 
@@ -9,6 +10,7 @@ app.use(express.json());
 database.connectToDatabase();
 
 app.use("/usuarios", userRouter); //rotas dos usuários
+app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
     res.send("Bem-Vindo ao nosso Market-Place");
